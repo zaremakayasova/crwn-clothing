b6b3cf1caa702c0e06163330c426eb27fcd8d694
Zarema <zaremakayasova@gmail.com>
2021-02-18 16:10:08 -0500

    updating our application with reselect, replaced cart selectors with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,22 @@
+//Selectors can provide performance optimizations 
+//so our components are not getting rerendered whenever state changes that unrelated to our component(if user state chnages, our item components dont get rerendered)
+//A selector is a function that accepts Redux state as an argument
+// and returns data that is derived from that state.
+
+import { createSelector } from "reselect";
+
+const selectCart = state => state.cart; //input selector
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems //second argument is a function that will return value we want out of this selector
+    //parameter of this function is output of input selector in the array
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumulatedQuantity, cartItem) =>
+                accumulatedQuantity + cartItem.quantity, 0)
+);
